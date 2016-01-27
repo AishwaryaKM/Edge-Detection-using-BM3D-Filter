e80b80ab4f02e67bbf964d3b02d65d792fa8b7e8
K M Aishwarya <AishwaryaKM@users.noreply.github.com>
2016-01-27 23:46:49 +0530

    Update colorspace.m

--- colorspace.m
+++ colorspace.m
@@ -105,30 +105,25 @@ return;
 
 
 function T = gettransform(Space)
-% Get a colorspace transform: either a matrix describing an affine transform,
+% A colorspace transform is obtained: either a matrix describing an affine transform,
 % or a string referring to a conversion subroutine
 switch Space
 case 'ypbpr'
    T = [0.299,0.587,0.114,0;-0.1687367,-0.331264,0.5,0;0.5,-0.418688,-0.081312,0];
 case 'yuv'
-   % R'G'B' to NTSC/PAL YUV
-   % Wikipedia: http://en.wikipedia.org/wiki/YUV
+
    T = [0.299,0.587,0.114,0;-0.147,-0.289,0.436,0;0.615,-0.515,-0.100,0];
 case 'ydbdr'
-   % R'G'B' to SECAM YDbDr
-   % Wikipedia: http://en.wikipedia.org/wiki/YDbDr
+
    T = [0.299,0.587,0.114,0;-0.450,-0.883,1.333,0;-1.333,1.116,0.217,0];
 case 'yiq'
-   % R'G'B' in [0,1] to NTSC YIQ in [0,1];[-0.595716,0.595716];[-0.522591,0.522591];
-   % Wikipedia: http://en.wikipedia.org/wiki/YIQ
+
    T = [0.299,0.587,0.114,0;0.595716,-0.274453,-0.321263,0;0.211456,-0.522591,0.311135,0];
 case 'ycbcr'
-   % R'G'B' (range [0,1]) to ITU-R BRT.601 (CCIR 601) Y'CbCr
-   % Wikipedia: http://en.wikipedia.org/wiki/YCbCr
-   % Poynton, Equation 3, scaling of R'G'B to Y'PbPr conversion
+
    T = [65.481,128.553,24.966,16;-37.797,-74.203,112.0,128;112.0,-93.786,-18.214,128];
 case 'jpegycbcr'
-   % Wikipedia: http://en.wikipedia.org/wiki/YCbCr
+  
    T = [0.299,0.587,0.114,0;-0.168736,-0.331264,0.5,0.5;0.5,-0.418688,-0.081312,0.5]*255;
 case {'rgb','xyz','hsv','hsl','lab','luv','lch'}
    T = Space;
